6e3ab1873f199b9771af822beeb0cd939fa42bcd
noise2 <b.g.dariush@gmail.com>
2017-03-19 22:07:59 +0330

    @ config/routes.rb: a fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,32 @@
-    json_request_only = {
-      defaults:    { format: :json }, 
-      constraints: { format: :json }
-    }
-    
-    ajax_server = {
-      only: [:create, :destroy]
-    }.merge(json_request_only)
-    
-    Rails.application.routes.draw do
-      
-      root to: 'home#index'
-      
-      get 'contact/us/:section', to: 'home#contactus', as: 'contact_us'
-      
-      resources :uploaded_files, ajax_server
-      
-      # only to create/delete shopping carts and only excepts JSON format
-      resources :shopping_carts, ajax_server
-      
-      resources :furniture_types, :path => "category" do
-        member do 
-          delete :delete_image
-          delete :archive
-          patch :recover
-        end
-      end
-      resources :furnitures
-      resources :profiles
-      devise_for :users
+json_request_only = {
+  defaults:    { format: :json }, 
+  constraints: { format: :json }
+}
+
+ajax_server = {
+  constraints: lambda { |request| request.xhr? },
+  only: [:create, :destroy]
+}.merge(json_request_only)
+
+Rails.application.routes.draw do
+  
+  root to: 'home#index'
+  
+  get 'contact/us/:section', to: 'home#contactus', as: 'contact_us'
+  
+  resources :uploaded_files, ajax_server
+  
+  # only to create/delete shopping carts and only excepts JSON format
+  resources :shopping_carts, ajax_server
+  
+  resources :furniture_types, :path => "category" do
+    member do 
+      delete :delete_image
+      delete :archive
+      patch :recover
     end
+  end
+  resources :furnitures
+  resources :profiles
+  devise_for :users
+end
