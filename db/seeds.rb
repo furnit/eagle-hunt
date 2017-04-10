71f06e9437cb1f115cedb7e7ff0629fb96e1ecbb
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-04-10 17:33:54 +0430

    [#143376229] @ db/seeds.rb: updated and improved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user_types_list = [
+
+#
+# => ading user types
+#
+[
   ['ادمین', 'ADMIN', 'ادمین سایت، دسترسی کامل به کل سایت' ,1001],
   ['مشتری', 'CLIENT','مشتری سایت که دسترسی عمومی به محتویات سایت دارد',1],
   ['فنی', 'FANI','موجودیت خیاط کار، امکان دسترسی به ارزیابی اطلاعات جزییات ساخت مبل',2],
@@ -16,31 +20,60 @@ user_types_list = [
   ['بازرس', 'PR','بازرسی کیفی فعالیت‌های بازاریاب‌ها، روابط عمومی مشتری‌ها',3],
   ['بازارسنج', 'MARKLINE','مسئول بروز رسانی قیمت‌های کالاها و لوازم پیش‌نیاز محصولات خدماتی',2]
 ]
-
-user_types_list.each do |name, symbol, comment, auth_level|
+.each do |name, symbol, comment, auth_level|
   Admin::UserType.create(name: name, symbol: symbol, comment: comment, auth_level: auth_level)
 end
 
-users_list = [
-  ['09120686119','123456',1, true],
-]
-users_list.each do |phone_number, password, user_type_id, change_password|
-  User.create!(phone_number: phone_number, password: password, admin_user_type_id: user_type_id, change_password: change_password)
-end
-
-profiles_list = [
-  ['داریوش', 'حسن‌پور', 'فولادشهر', 1]
-]
-profiles_list.each do |first_name, last_name, address, user_id|
-  Profile.create!(first_name: first_name, last_name: last_name, address: address, user_id: user_id)
-end
-
-states_list = [
+#
+# => adding states
+#
+[
   'آذربایجان شرقی', 'آذربایجان غربی', 'اردبیل', 'اصفهان', 'البرز', 'ایلام', 'بوشهر', 'تهران', 'خراسان جنوبی', 'خراسان رضوی', 'خراسان شمالی', 'خوزستان', 'زنجان',
   'سمنان', 'سیستان و بلوچستان', 'فارس', 'قزوین', 'قم', 'لرستان', 'مازندران', 'مرکزی', 'هرمزگان', 'همدان', 'چهارمحال و بختیاری', 'کردستان', 'کرمان', 'کرمانشاه',
   'کهگیلویه و بویراحمد', 'گلستان', 'گیلان', 'یزد'
 ]
-
-states_list.each do |name|
+.each do |name|
   State.create!(name: name)
+end
+
+#
+# => adding admins
+#
+[
+  '09120686119',
+]
+.each do |phone_number|
+  User.create!(
+    phone_number: phone_number,
+    password: phone_number,
+    password_confirmation: phone_number,
+    admin_user_type_id: Admin::UserType.where('symbol = ?', :ADMIN.to_s).first.id,
+    change_password: true
+  )
+end
+
+#
+# => adding admins' profile
+#
+[
+  ['داریوش', 'حسن‌پور', 'اصفهان']
+]
+.each.with_index do |data, index|
+  Profile.create!(first_name: data[0], last_name: data[1], state_id: State.where('name = ?', data[2]).first.id, address: data[2], user_id: index + 1)
+end
+
+#
+# => adding furniture section details
+#
+[
+  ['پشتی', 'به قسمت تکیه‌گاه مبل گفته می‌شود.', "data/images/sofa/poshti.jpg"],
+  ['زیری', 'به قسمت تحتانی و نشیمنگاه مبل گفته می‌شود.', "data/images/sofa/ziri.jpg"],
+  ['دسته', 'به دسته‌های مبل گفته می‌شود.', "data/images/sofa/daste.jpg"],
+  ['پایه', 'به پایه‌های مبل اطلاق می‌شود.', "data/images/sofa/paye.jpg"],
+  ['کوسن', 'به بالشتک‌های مبل گفته‌ می‌شود.', "data/images/sofa/kosan.jpg"]
+]
+.each do |name, comment, image|
+  section = Admin::FurnitureSection.create!(name: name, comment: comment)
+  section.images = [Rails.root.join(image).open]
+  section.save!
 end
