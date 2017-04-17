1f89891d6929dadfa4bf081d5ab6c3ad17a7d50e
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-04-17 17:09:32 +0430

    [#143775055] @ notify-me subscription done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,15 +16,34 @@ class HomeController < ApplicationController
 	end
 	
 	def furniture_notify
+	  # verify the recaptcha
 	  if not verify_recaptcha
       respond_to do |format|
         format.json { render json: {message: "احراز هویت به درستی صورت نگرفته است، لطفا دوباره تلاش کنید."}, status: :unprocessable_entity }
       end
       return
 	  end
+	  
+	  success_notice = 'شماره شما با موفقیت ثبت گردید.' 
+	  
+	  # either the user is signed in and we obtain the number from the profile or get it from form
+	  phone_number = user_signed_in? ? current_user.phone_number : params[:phone_number]
+	  
+	  # all deletions are valid by default!
+	  valid = !params[:checked].to_boolean;
+	  
+	  # if we should add it to database?
+	  if params[:checked].to_boolean
+	    valid = NotifyOnFurnitureAvailable.find_or_create_by(phone_number: phone_number, admin_furniture_id: params[:fid]).errors.empty?
+	  else
+	    # we should delete from database
+	    success_notice = 'شماره شما با موفقیت از لیست پیگیری‌های این محصول حذف گردید.' 
+	    NotifyOnFurnitureAvailable.delete_all(phone_number: current_user.phone_number, admin_furniture_id: params[:fid]) if user_signed_in?
+	  end
+	  
     respond_to do |format|
-  	  if NotifyOnFurnitureAvailable.find_or_create_by(phone_number: params[:phone_number], admin_furniture_id: params[:fid])
-  	    format.json { render json: {message: 'شماره شما با موفقیت ثبت گردید.'}, status: :ok }
+  	  if valid 
+  	    format.json { render json: {message: success_notice}, status: :ok }
 	    else
   	    format.json { render json: {message: "خطا در ثبت اطلاعات، لطفا دوباره تلاش کنید."}, status: :unprocessable_entity }
   	  end
--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -2,4 +2,8 @@ class String
   def to_ar2en_i
     self.tr!('۰۱۲۳۴۵۶۷۸۹','0123456789') || self
   end
+  
+  def to_boolean
+    self == 'true' || self == '1'
+  end
 end
