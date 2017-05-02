5fd602e2d7314d89c31e7e879d5be45d83c99f4e
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-05-02 16:02:46 +0430

    [#144587009] @ app/controllers/admin/fabrics_controller.rb: done

--- app/controllers/admin/fabrics_controller.rb
+++ app/controllers/admin/fabrics_controller.rb
@@ -1,5 +1,5 @@
 class Admin::FabricsController < Admin::UploaderController
-  before_action :set_admin_fabric, only: [:show, :edit, :update, :list_images, :archive]
+  before_action :set_admin_fabric, only: [:show, :edit, :update, :archive]
 
   # GET /admin/fabrics
   # GET /admin/fabrics.json
@@ -15,10 +15,12 @@ class Admin::FabricsController < Admin::UploaderController
   # GET /admin/fabrics/new
   def new
     @admin_fabric = Admin::Fabric.new
+    consider_parital_view
   end
 
   # GET /admin/fabrics/1/edit
   def edit
+    consider_parital_view
   end
 
   # POST /admin/fabrics
@@ -31,8 +33,8 @@ class Admin::FabricsController < Admin::UploaderController
         # update the uploaded image and re-save the model
         # the model need to be created at first then the update happen
         update_uploaded_images @admin_fabric, :admin_fabric, auto_save: true
-        format.html { redirect_to @admin_fabric, notice: "طرح «<b>#{@admin_fabric.id}</b>» با موفقیت ایجاد شد." }
-        format.json { render json: @admin_fabric, status: :created, location: @admin_fabric }
+        format.html { redirect_to admin_fabrics_path, notice: "طرح «<b>#{@admin_fabric.id}</b>» با موفقیت ایجاد شد." }
+        format.json { render json: @admin_fabric, status: :created, location: admin_fabrics_path }
       else
         format.html { render :new }
         format.json { render json: @admin_fabric.errors, status: :unprocessable_entity }
@@ -48,8 +50,8 @@ class Admin::FabricsController < Admin::UploaderController
     update_uploaded_images @admin_fabric, :admin_fabric
     respond_to do |format|
       if @admin_fabric.update(admin_fabric_params)
-        format.html { redirect_to @admin_fabric, notice: "طرح «<b>#{@admin_fabric.id}</b>» با موفقیت ویرایش شد." }
-        format.json { render json: @admin_fabric, status: :ok, location: @admin_fabric }
+        format.html { redirect_to admin_fabrics_path, notice: "طرح «<b>#{@admin_fabric.id}</b>» با موفقیت ویرایش شد." }
+        format.json { render json: @admin_fabric, status: :ok, location: admin_fabrics_path }
       else
         format.html { render :edit }
         format.json { render json: @admin_fabric.errors, status: :unprocessable_entity }
@@ -66,7 +68,7 @@ class Admin::FabricsController < Admin::UploaderController
     # delete the record
     @admin_fabric.destroy_fully!
     respond_to do |format|
-      format.html { redirect_to admin_fabrics_url, notice: "طرح «<b>#{@admin_fabric.id}</b>» با موفقیت حذف شد." }
+      format.html { redirect_to admin_fabrics_path, notice: "طرح «<b>#{@admin_fabric.id}</b>» با موفقیت حذف شد." }
       format.json { head :no_content }
     end
   end
@@ -99,17 +101,31 @@ class Admin::FabricsController < Admin::UploaderController
   end
 
   def list_images
-    ls_images @admin_fabric
+    ls_images Admin::Fabric.with_deleted.find(params[:id])
   end
 
   private
+  
+    def consider_parital_view
+      return if not params[:pv]
+      id = params[:pv].to_i
+      partials = [
+        '01_general',
+        '02_images_detail'
+      ]
+      if partials[id]
+        render partial: "admin/fabrics/forms/#{partials[id]}"
+      end
+    end
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_admin_fabric
       @admin_fabric = Admin::Fabric.find(params[:id])
+      @admin_fabric.images_detail ||= []
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_fabric_params
-      params.require(:admin_fabric).permit(:admin_fabric_type_id, :admin_fabric_brand_id, :comment, :images, :images_detail)
+      params.require(:admin_fabric).permit(:admin_fabric_type_id, :admin_fabric_brand_id, :comment, { images_detail: [] })
     end
 end
--- app/models/admin/fabric.rb
+++ app/models/admin/fabric.rb
@@ -3,6 +3,9 @@ class Admin::Fabric < ParanoiaRecord
   belongs_to :brand, foreign_key: :admin_fabric_brand_id, class_name: '::Admin::FabricBrand'
   
   mount_uploaders :images, ImageUploader
+  # don't delete the images on soft delete
+  # see: (github.com/carrierwaveuploader/carrierwave/issues/624#issuecomment-15243440)
+  skip_callback :commit, :after, :remove_images!
   
   validates_presence_of :admin_fabric_type_id, :admin_fabric_brand_id
 end
