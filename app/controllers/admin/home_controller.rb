0130abb023e011021268d143b9d76dc4d47e732d
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-05-17 17:02:43 +0430

    @ app/controllers/admin/home_controller.rb: a syntax fix!

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,11 +1,17 @@
 class Admin::HomeController < Admin::AdminbaseController
   # keep flashes for sub-controllers' view to handle
-  before_action { flash.keep; flash[:keeped] = true }, only: [:index]
+  before_action :keep_flash, only: [:index]
   
   def index
     render :layout => 'application'
   end
   
   def dashboard
+  end
+  
+  protected
+  
+  def keep_flash
+    flash.keep; flash[:keeped] = true 
   end
 end
