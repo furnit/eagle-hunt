89c1c0366731bac711e1e57f578ebc6fab91e96d
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-05-26 14:40:29 +0430

    [#142219125] @ spec/factories/backbag.rb: improved text

--- spec/factories/backbag.rb
+++ spec/factories/backbag.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     ((0..9).map { ('a'..'z').to_a[rand(26)] }.join + n.to_s)
   end
   sequence :text do |n|
-    byebug
     Faker::Lorem.paragraphs(3).join("[#{n.to_s}]. ")
   end
   sequence :email do |n|
