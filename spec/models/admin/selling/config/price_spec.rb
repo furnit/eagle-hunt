56c81faf3e0f913343cc0f6e99dda21b2b8c9218
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-06-04 12:33:19 +0430

    [#142219125] @ spec/models/admin/selling/config/price_spec.rb: trying to fix foreignkey issue on save

--- spec/models/admin/selling/config/price_spec.rb
+++ spec/models/admin/selling/config/price_spec.rb
@@ -22,12 +22,40 @@ RSpec.describe Admin::Selling::Config::Price, type: :model do
       subject.wood = nil
       expect(subject).to have_errors_on :wood, errors: :blank
     end
+
+    context "when is not valid" do
+      it "raises exception" do
+        subject.wood = Admin::Furniture::Wood::Type.new(id: -1)
+        expect { subject.save }.to raise_error ActiveRecord::InvalidForeignKey, /.*FOREIGN KEY \(`admin_furniture_wood_type_id`\) REFERENCES `admin_furniture_wood_types` \(`id`\).*/
+      end
+    end
   end
 
-  describe "#fabric" do
+  describe "#fabric_brand" do
     it "is required" do
-      subject.fabric = nil
-      expect(subject).to have_errors_on :fabric, errors: :blank
+      subject.fabric_brand = nil
+      expect(subject).to have_errors_on :fabric_brand, errors: :blank
+    end
+
+    context "when is not valid" do
+      it "raises exception" do
+        subject.fabric_brand = Admin::Furniture::Fabric::Brand.new(id: -1)
+        expect { subject.save }.to raise_error ActiveRecord::InvalidForeignKey, /.*FOREIGN KEY \(`admin_furniture_fabric_brand_id`\) REFERENCES `admin_furniture_fabric_brands` \(`id`\).*/
+      end
+    end
+  end
+
+  describe "#paint_color" do
+    it "is required" do
+      subject.paint_color = nil
+      expect(subject).to have_errors_on :paint_color, errors: :blank
+    end
+
+    context "when is not valid" do
+      it "raises exception" do
+        subject.paint_color = Admin::Furniture::Paint::ColorBrand.new(id: -1)
+        expect { subject.save }.to raise_error ActiveRecord::InvalidForeignKey, /.*FOREIGN KEY \(`admin_furniture_paint_color_brand_id`\) REFERENCES `admin_furniture_paint_color_brands` \(`id`\).*/
+      end
     end
   end
 
@@ -36,12 +64,19 @@ RSpec.describe Admin::Selling::Config::Price, type: :model do
       subject.furniture = nil
       expect(subject).to have_errors_on :furniture, errors: :blank
     end
-  end
 
-  describe "#paint_color" do
-    it "is required" do
-      subject.paint_color = nil
-      expect(subject).to have_errors_on :paint_color, errors: :blank
+    context "when is not valid" do
+      it "raises exception" do
+        subject.furniture = Admin::Furniture::Furniture.new(id: -1)
+        expect { subject.save }.to raise_error ActiveRecord::InvalidForeignKey, /.*FOREIGN KEY \(`admin_furniture_furniture_id`\) REFERENCES `admin_furniture_furnitures` \(`id`\).*/
+      end
+    end
+
+    context "when is not unique" do
+      it "is not valid" do
+        expect(subject.save).to be_truthy
+        expect(subject.dup).to have_errors_on :furniture, errors: :taken
+      end
     end
   end
 end
