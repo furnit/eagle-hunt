37c4a5ebb345ddf8bf5a6a7a2f83c51312285efb
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-06-16 13:25:04 +0430

    [#142219125] @ app/models/admin/selling/config/price.rb: a fix but has errors in its spec, pinned by `byebug`

--- app/models/admin/selling/config/price.rb
+++ app/models/admin/selling/config/price.rb
@@ -5,6 +5,10 @@ class Admin::Selling::Config::Price < ApplicationRecord
   belongs_to :fabric_brand, class_name: "::Admin::Furniture::Fabric::Brand", foreign_key: :admin_furniture_fabric_brand_id
   belongs_to :paint_color, class_name: "::Admin::Furniture::Paint::ColorBrand", foreign_key: :admin_furniture_paint_color_brand_id
   
+  # don't validate presence of the attributes, introduced from commit@13426a0
+  #   the issue raised testing the site from at commit@1b64071 which the at the method 
+  #   `Admin::Selling::Config::PricesController.set_admin_selling_config_price()` when trying to `.find_or_create()` this model
+  #   validation will fail and the site won't perform! 
   # validates_presence_of :overall_cost, :set, :wood, :fabric_brand, :furniture, :paint_color
   validates_numericality_of :overall_cost, greater_than: 0, allow_nil: true
   validates_uniqueness_of :furniture
--- spec/models/admin/selling/config/price_spec.rb
+++ spec/models/admin/selling/config/price_spec.rb
@@ -1,26 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Admin::Selling::Config::Price, type: :model do
-  subject { build :admin_selling_config_price }
+  subject {
+    puts "-----------------------------------------------------"
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    build :admin_selling_config_price
+  }
 
   describe "#overall_cost" do
-    it "is required" do
+    it "is optional" do
       subject.overall_cost = nil
-      expect(subject).to have_errors_on :overall_cost, errors: :blank
+      expect(subject).to be_valid_on :overall_cost
+    end
+
+    context "when is zero" do
+      it "is valid" do
+        subject.overall_cost = 1
+        expect(subject).to be_valid_on :overall_cost
+      end
     end
 
     context "when is negative" do
       it "is not valid" do
-        subject.overall_cost = -1
-        expect(subject).to have_errors_on :overall_cost, errors: :greater_than
+        (-1..0).each do |i|
+          subject.overall_cost = i
+          expect(subject).to have_errors_on :overall_cost, errors: :greater_than
+        end
       end
     end
   end
 
   describe "#wood" do
-    it "is required" do
+    it "is optional" do
       subject.wood = nil
-      expect(subject).to have_errors_on :wood, errors: :blank
+      expect(subject).to be_valid_on :wood
     end
 
     context "when is not valid" do
@@ -34,7 +47,7 @@ RSpec.describe Admin::Selling::Config::Price, type: :model do
   describe "#fabric_brand" do
     it "is required" do
       subject.fabric_brand = nil
-      expect(subject).to have_errors_on :fabric_brand, errors: :blank
+      expect(subject).to be_valid_on :fabric_brand
     end
 
     context "when is not valid" do
@@ -48,7 +61,7 @@ RSpec.describe Admin::Selling::Config::Price, type: :model do
   describe "#paint_color" do
     it "is required" do
       subject.paint_color = nil
-      expect(subject).to have_errors_on :paint_color, errors: :blank
+      expect(subject).to be_valid_on :paint_color
     end
 
     context "when is not valid" do
@@ -62,7 +75,7 @@ RSpec.describe Admin::Selling::Config::Price, type: :model do
   describe "#furniture" do
     it "is required" do
       subject.furniture = nil
-      expect(subject).to have_errors_on :furniture, errors: :blank
+      expect(subject).to be_valid_on :furniture
     end
 
     context "when is not valid" do
@@ -74,6 +87,7 @@ RSpec.describe Admin::Selling::Config::Price, type: :model do
 
     context "when is not unique" do
       it "is not valid" do
+        byebug
         expect(subject.save).to be_truthy
         expect(subject.dup).to have_errors_on :furniture, errors: :taken
       end
