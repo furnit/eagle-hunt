36874f3bcd8edc6b846dc422980984643398d2c1
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-08-01 13:00:14 +0430

    @ spec/factories/admin_furniture_sets.rb: a fix

--- spec/factories/admin_furniture_sets.rb
+++ spec/factories/admin_furniture_sets.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :admin_furniture_set, class: 'Admin::Furniture::Set' do
-    before(:create) do |set|
+    after(:build) do |set|
       set.config.each do |s|
         # going for `.save` to prevent from having
         # `ActiveRecord::RecordInvalid` error on `create` for duplicate `#piece`
