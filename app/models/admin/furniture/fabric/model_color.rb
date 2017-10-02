d05b159691fea3f7acb1a2049f6a4a3f0baf8ff3
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-10-03 00:49:22 +0330

    [#142216919] @ app/models/admin/furniture/fabric/model_color.rb: fix for issue from commit@a6eb48d

--- app/models/admin/furniture/fabric/model_color.rb
+++ app/models/admin/furniture/fabric/model_color.rb
@@ -2,6 +2,6 @@ class Admin::Furniture::Fabric::ModelColor < ApplicationRecord
   belongs_to :model, foreign_key: :admin_furniture_fabric_model_id, class_name: '::Admin::Furniture::Fabric::Model'
   belongs_to :color, foreign_key: :admin_furniture_fabric_color_id, class_name: '::Admin::Furniture::Fabric::Color'
 
-  validates_presence_of :models, :color
-  validates_uniqueness_of :models, scope: :color
+  validates_presence_of :model, :color
+  validates_uniqueness_of :model, scope: :color
 end
