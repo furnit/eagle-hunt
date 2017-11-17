56e81c200e373510c5df1ecf297d3820781b7526
Dariush Hasanpour <b.g.dariush@gmail.com>
2017-11-17 10:35:10 +0330

    [#142216919] @ app/assets/javascripts/vendors/nprogress-ajax-fix.js: a fix

--- app/assets/javascripts/vendors/nprogress-ajax-fix.js
+++ app/assets/javascripts/vendors/nprogress-ajax-fix.js
@@ -1,4 +1,6 @@
 jQuery(function() {
+	// fail-safe
+	if(typeof document.NProgress === "undefined") document.NProgress = window.NProgress;
   jQuery(document).on('ajaxStart', function(){ document.NProgress.start(); });
   jQuery(document).on('ajaxStop', function(){ document.NProgress.done(); });
 });
